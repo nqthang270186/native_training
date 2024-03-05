a455d43b52a94b05c9a9b1b9509e48b0850155b0
nguyen quoc thang <>
2024-03-05 13:28:45 +0700

    Add existing project files prior to the push to GitHub.

---
+++ NativeTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NativeTraining
+//
+//  Created by nguyen quoc thang on 05/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
